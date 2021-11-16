dfc11a308c5f864cb1db4fa6d2ad6c6982cf0be7
Aleisi <86322128+Aleisi@users.noreply.github.com>
2021-11-15 21:57:09 -0600

    authentication of login and sign up ready

--- notes-for-you/src/App.js
+++ notes-for-you/src/App.js
@@ -1,23 +1,22 @@
-import React from "react";
+// import React from "react";
 import './App.css';
-import { BrowserRouter, Route } from "react-router-dom";
-import { Switch } from "react-router";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound";
-// import "./styles/Home.css";
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Switch>
         <Route exact path="/" component={Login} />
         <Route path="/Home" component={Home} />
         <Route path="/SignUp" component={SignUp} />
-        <Route component={PageNotFound}></Route>
+        <Route component={PageNotFound}/>
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
 
